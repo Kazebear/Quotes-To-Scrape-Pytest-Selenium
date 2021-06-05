10af16c68e34bf2ebb35839ae10f1132813e0e56
Kazebear <kazee.bear@mail.com>
2021-06-06 00:07:17 +0300

    v3

--- BookEStoreTest/TestsBookstore.py
+++ BookEStoreTest/TestsBookstore.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+linktexts = ["Books", "Travel", "Mystery", "Historical Fiction", "Sequential Art", "Classics", "Philosophy",
+             "Romance", "Womens Fiction", "Fiction", "Childrens", "Religion", "Nonfiction", "Music",
+             "Default", "Science Fiction", "Sports and Games", "Add a comment", "Fantasy", "New Adult",
+             "Young Adult", "Science", "Poetry", "Paranormal", "Art", "Psychology", "Autobiography",
+             "Parenting", "Adult Fiction", "Humor", "Horror", "History", "Food and Drink", "Christian Fiction",
+             "Business", "Biography", "Thriller", "Contemporary", "Spirituality", "Academic", "Self Help",
+             "Historical",
+             "Christian", "Suspense", "Short Stories", "Novels", "Health", "Politics", "Cultural", "Erotica",
+             "Crime"]
+
 
 def test_home_page():
     driver = webdriver.Chrome('/Users/kaze/Desktop/chromedriver/chromedriver')
@@ -9,10 +19,34 @@ def test_home_page():
     driver.close()
 
 
-def test_travel_page():
+def test_pages_nav():
     driver = webdriver.Chrome('/Users/kaze/Desktop/chromedriver/chromedriver')
     driver.get('http://books.toscrape.com')
-    travel_link = driver.find_element_by_partial_link_text('Trav')
-    travel_link.send_keys(Keys.RETURN)
-    assert "Travel" in driver.title
+    driver.implicitly_wait(10)
+    for i in linktexts:
+        stri = "".join(i)
+        driver.implicitly_wait(3)
+        section = driver.find_element_by_link_text(stri)
+        section.send_keys(Keys.RETURN)
+        driver.implicitly_wait(3)
+        assert stri in driver.title
+        stri.replace(stri, "")
+
+    driver.close()
+
+
+def test_pages_books_row():
+    driver = webdriver.Chrome('/Users/kaze/Desktop/chromedriver/chromedriver')
+    driver.get('http://books.toscrape.com')
+    driver.implicitly_wait(10)
+    for i in linktexts:
+        stri = "".join(i)
+        driver.implicitly_wait(3)
+        section = driver.find_element_by_link_text(stri)
+        section.send_keys(Keys.RETURN)
+        driver.implicitly_wait(3)
+        contents = driver.find_element_by_class_name("row")
+        assert contents.is_displayed()
+        stri.replace(stri, "")
+
     driver.close()
